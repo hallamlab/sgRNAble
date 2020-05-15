f1b698f7ae3b08c7825a4ce3ad74ba32057d1266
Siddarth <sidr97@gmail.com>
2020-05-14 21:05:12 -0700

    Rank Support
    Ranked each guide based on its entropy score for every target gene

--- optimal_guide_finder/guide_finder.py
+++ optimal_guide_finder/guide_finder.py
@@ -11,6 +11,8 @@ from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
 import guide_generator
 import guide_strength_calculator
+import pandas as pd
+import numpy as np
 
 FASTA_FILE = "../output/Run_Genome_Plus_RC"
 
@@ -83,7 +85,10 @@ def main():
     # Get the sequences in a Seq format from user fasta or genebank files
     target_dict, genome = get_sequence(args)
 
-    ref_record = SeqRecord(genome, id="refgenome", name="reference", description="a reference background")
+    ref_record = SeqRecord(genome,
+                           id="refgenome", 
+                           name="reference", 
+                           description="a reference background")
     ref_record = ref_record + ref_record.reverse_complement()
     SeqIO.write(ref_record, FASTA_FILE, "fasta")
 
@@ -91,7 +96,14 @@ def main():
     guide_list = guide_generator.select_guides(target_dict, args)
     # Build and run the model
     guide_strength_calculator.initialize_logger(args.output_name)
-    results_df = guide_strength_calculator.initalize_model(guide_list, FASTA_FILE, num_threads=args.threads)
+
+    results_df = guide_strength_calculator.initalize_model(guide_list, 
+                                                           FASTA_FILE,
+                                                           num_threads=args.threads)
+    rank_array = np.repeat(np.arange(1,args.azimuth_cutoff+1), 
+                           len(results_df['Gene/ORF Name'].unique())) 
+    results_df.sort_values(by=['Gene/ORF Name','Entropy Score'], inplace=True)
+    results_df['Rank in Target Gene'] = rank_array
 
     results_df.to_csv("../output/" + args.output_name + ".csv", index=False)
 
